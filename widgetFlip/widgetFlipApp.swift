a658896c19651d474e6fd1cd0052f6ef96c918f5
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2025-12-27 01:47:55 +0300

    Initial Commit

---
+++ widgetFlip/widgetFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetFlipApp.swift
+//  widgetFlip
+//
+//  Created by Akinalp Fidan on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct widgetFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
